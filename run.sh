cca2dbd71efdc543c4673ff4375bbfc40e119803
moongni <moongni@github.com>
2023-08-03 08:55:21 +0000

    Fix: Change variable path

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ HOST="0.0.0.0"
 PORT="8080"
 export BACKEND_URL="http://63.35.31.27:8000"
 export VIDEO_DIR="/workspace/data/video"
+export WORKING_DIR="/workspace/temp"
 
 # The program is run with the following command:
 exec uvicorn --reload --host "$HOST" --port "$PORT" "$APP_MODULE"
