5a58a7732884edbde2c47886bdbe14ea11c43294
moongni <moongni@github.com>
2023-08-03 06:03:59 +0000

    Fix: change run.sh file & add os variable

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ export PYTHONPATH="$(pwd)"
 APP_MODULE="main:app"
 HOST="0.0.0.0"
 PORT="8080"
+export BACKEND_URL="http://63.35.31.27:8000"
+export VIDEO_DIR="/workspace/data/video"
 
 # The program is run with the following command:
 exec uvicorn --reload --host "$HOST" --port "$PORT" "$APP_MODULE"
